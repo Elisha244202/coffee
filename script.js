d91f24a72f9f8e0386ade1d056d8e7302dd89160
Elisha244202 <160017098+Elisha244202@users.noreply.github.com>
2024-02-16 13:37:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,22 @@ const navbar = document.querySelector(".navbar");
 
 document.querySelector("#menu-btn").onclick = function(){
   navbar.classList.toggle("active");
+  cartItem.classList.remove("active");
+  searchForm.classList.remove("active");
 };
 
 const cartItem = document.querySelector(".header .cart-items-container");
 
 document.querySelector("#cart-btn").onclick = function(){
   cartItem.classList.toggle("active");
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
 };
 
 const searchForm = document.querySelector(".header .search-form");
 
 document.querySelector("#search-btn").onclick = function(){
   searchForm.classList.toggle("active");
+  navbar.classList.remove("active");
+  cartItem.classList.remove("active");
 };
